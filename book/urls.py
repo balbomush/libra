ffd6de7f951bd425c3c6ff95a2f535ad3304521c
Alex <a.lexich@mail.ru>
2017-11-05 21:40:41 +0300

    modified templates 2

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.names, name='name'),
+    url(r'^book/(?P<pk>\d+)/$', views.book_detail, name='book_detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Book
 
 def names(request):
     names = Book.objects.filter(author='Достоевский').order_by('published_date')
     return render(request, 'book/name.html', {'names': names})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'book/book_detail.html', {'book': book})
