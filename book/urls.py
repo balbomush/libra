fa3c9dbc9e144126e748c9d4f874591d7e3af171
Alex <a.lexich@mail.ru>
2017-11-05 23:52:17 +0300

    final after djgirls

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.names, name='name'),
     url(r'^book/(?P<pk>\d+)/$', views.book_detail, name='book_detail'),
+    url(r'',views.book_new, name='new_book')
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Book
+from .forms import PostForm
 
 def names(request):
     names = Book.objects.filter(author='Достоевский').order_by('published_date')
@@ -9,3 +10,14 @@ def names(request):
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'book/book_detail.html', {'book': book})
+
+def book_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.save()
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = PostForm()
+    return render(request, 'book/book_edit.html', {'form': form})
